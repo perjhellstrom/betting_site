443a1b24fc7af953352856349b05c985158c20a2
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2018-01-27 15:48:38 +0100

    Even more work

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from forms import MyForm
 
 app = Flask("betting_app")
+app.config["WTF_CSRF_ENABLED"] = False
 
 @app.route("/")
 def home():
     return render_template("index.html", heading="Hello!")
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    form = MyForm()
+    if form.validate_on_submit():
+        return "Hey there, {}!".format(form.name.data)
+    else:
+        return render_template("contact.html", form=form)
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
