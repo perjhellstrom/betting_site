dd876e727bfdfe374df7d81b091b9761978e28a2
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2018-02-16 21:22:18 +0100

    Remove jQuery from detailed_match.js

--- javascript/detailed_match.js
+++ javascript/detailed_match.js
@@ -6,20 +6,18 @@ function DetailedMatchWindow(element) {
 	this.isVisible = false;
 
 	this.hide = function() {
-		this.element.hide();
+		this.element.style.visibility = "hidden";
 		this.isVisible = false;
 	}
 
 	this.show = function() {
-		this.element.show();
+		this.element.style.visibility = "visible";
 		this.isVisible = true;
 	}
 
 	this.update = function() {
-		this.element.css({
-			top: this.position.top - this.scroll.top, 
-			left: this.position.left - this.scroll.left
-		});
+		this.element.style.top = (this.position.top - this.scroll.top) + "px";
+		this.element.style.left = (this.position.left - this.scroll.left) + "px";
 	}
 
 	this.positionIsSame = function(position) {
@@ -28,36 +26,42 @@ function DetailedMatchWindow(element) {
 }
 
 var buildDetailedMatchWindow = function(bracketMatch) {
-	for(var index = 0; index < bracketMatch.children().length; index++)
+	for(var index = 0; index < bracketMatch.children.length; index++)
 	{
-		$("#detailed-match").children()[index].innerText = bracketMatch.children()[index].innerText;
+		var detailedMath = document.getElementById("detailed-match");
+		detailedMath.children[index].innerText = bracketMatch.children[index].innerText;
 	}
 }
 
-var detailedMatchWindow = new DetailedMatchWindow($("#detailed-match"));
+var detailedMatchWindow = new DetailedMatchWindow(document.getElementById("detailed-match"));
 detailedMatchWindow.hide();
 
-$(".bracket-match").click(function() {
-	var position = $(this).position();
-	position.left += $(this).width();
+var onBracketMatchClick = function() {
+	var position = {top:this.offsetTop, left:this.offsetLeft};
+	position.left += this.offsetWidth;
 	if (detailedMatchWindow.positionIsSame(position) && detailedMatchWindow.isVisible) {
 		detailedMatchWindow.hide();
 	} else {
 		detailedMatchWindow.show();
-		buildDetailedMatchWindow($(this));
+		buildDetailedMatchWindow(this);
 	}
 	detailedMatchWindow.position = position;
 	detailedMatchWindow.update();
-});
+}
 
-$(window).scroll(function() {
-	detailedMatchWindow.scroll.top = $(window).scrollTop();
-	detailedMatchWindow.scroll.left = $(window).scrollLeft();
+var bracketMatches = document.querySelectorAll(".bracket-match");
+for (var index = 0; index < bracketMatches.length; index++) {
+	bracketMatches[index].addEventListener("click", onBracketMatchClick);
+}
+
+window.addEventListener('scroll', function(event) {
+	detailedMatchWindow.scroll.top = document.documentElement.scrollTop;
+	detailedMatchWindow.scroll.left = document.documentElement.scrollLeft;
 	detailedMatchWindow.update();
 });
 
-$(window).resize(function() {
-	detailedMatchWindow.scroll.top = $(window).scrollTop();
-	detailedMatchWindow.scroll.left = $(window).scrollLeft();
+window.addEventListener('resize', function(event) {
+	detailedMatchWindow.scroll.top = document.documentElement.scrollTop;
+	detailedMatchWindow.scroll.left = document.documentElement.scrollLeft;
 	detailedMatchWindow.update();
 });
