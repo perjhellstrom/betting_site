be0779d197ed1a365129f120d55db260b93fdc95
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2018-02-24 00:41:24 +0100

    Complete tab, page support

--- site/javascript/page_switcher.js
+++ site/javascript/page_switcher.js
@@ -1,10 +1,38 @@
-
-var onTabClick = function(event) {
+var deactivateTabs = function() {
     var tabs = document.querySelectorAll(".tab");
     for (var index = 0; index < tabs.length; index++) {
         tabs[index].classList.remove("tab-active");
     }
+};
+
+var disablePages = function() {
+    var pages = document.querySelectorAll(".page");
+    for (var index = 0; index < pages.length; index++) {
+        console.log(pages[index].style.display);
+        pages[index].style.display = "none";
+    }
+};
+
+var enablePage = function(id) {
+    document.getElementById(id).style.display = "block";
+}
+
+var enableCurrentTabPage = function(tabId) {
+    switch (tabId) {
+        case "tournaments-button":
+            enablePage("tournaments-page");
+            break;
+        case "create-tournament-button":
+            enablePage("create-tournament-page");
+            break;
+    }
+}
+
+var onTabClick = function(event) {
+    deactivateTabs();
+    disablePages();
     event.target.classList.add("tab-active");
+    enableCurrentTabPage(event.target.id);
 }
 
 window.addEventListener("load", function(event) {
@@ -12,4 +40,9 @@ window.addEventListener("load", function(event) {
     for (var index = 0; index < tabs.length; index++) {
         tabs[index].addEventListener("click", onTabClick);
     }
+
+    deactivateTabs();
+    disablePages();
+    tabs[0].classList.add("tab-active");
+    enableCurrentTabPage(tabs[0].id);
 });
