e4ff64b8585fe7cee1c6242cbe6f8883a2934858
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2018-03-01 21:51:16 +0100

    Make it possible to dynamically add new stages when creating tournament

---
+++ site/javascript/stage_creation.js
@@ -0,0 +1,36 @@
+
+var addStage = function(event) {
+    var groupBeforeBracketPage = document.getElementById("group-before-bracket-page");
+    var creationStageContainer = groupBeforeBracketPage.querySelectorAll(".creation-stage-container")[0];
+    
+    var creationStage = appendElementOn(creationStageContainer, "div", "creation-stage");
+    var creationHeaderSection = appendElementOn(creationStage, "section", "");
+    var header = appendElementOn(creationHeaderSection, "h3", "");
+    header.innerText = "Group Stage " + creationStageContainer.children.length;
+
+    var creationControlPanel = appendElementOn(creationStage, "div", "creation-control-panel");
+    var playersPerGroupElement = appendElementOn(creationControlPanel, "div", "creation-control-panel-element");
+    var advancingPlayersElement = appendElementOn(creationControlPanel, "div", "creation-control-panel-element");
+    var addGroupElement = appendElementOn(creationControlPanel, "div", "creation-control-panel-element");
+
+    appendElementOn(playersPerGroupElement, "label", "").innerText = "Players per group:";
+    var playersPerGroupInput = appendElementOn(playersPerGroupElement, "input", "");
+    playersPerGroupInput.type = "number";
+    playersPerGroupInput.min = "0";
+
+    appendElementOn(advancingPlayersElement, "label", "").innerText = "Advancing players:";
+    var advancingPlayersInput = appendElementOn(advancingPlayersElement, "input", "");
+    advancingPlayersInput.type = "number";
+    advancingPlayersInput.min = "0";
+
+    var addGroupButton = appendElementOn(addGroupElement, "div", "creation-control-panel-button");
+    addGroupButton.innerText = "Add group";
+    addGroupButton.onclick = addGroup;
+
+    appendElementOn(creationStage, "div", "group-stage-container");
+};
+
+window.addEventListener("load", function(event) {
+    var addStageButton = document.getElementById("add-stage");
+    addStageButton.addEventListener("click", addStage);
+});
