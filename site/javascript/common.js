2b6d2218fc4bb747caca7e15b27cb0d70be9e086
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2018-03-01 21:46:08 +0100

    Create common script file

---
+++ site/javascript/common.js
@@ -0,0 +1,22 @@
+
+var appendElementOn = function(parent, elementType, selector) {
+    var element = document.createElement(elementType);
+    element.setAttribute("class", selector);
+    parent.appendChild(element);
+    return element;
+};
+
+var appendDivOn = function(parent, selector) {
+    var element = document.createElement("div");
+    element.setAttribute("class", selector);
+    parent.appendChild(element);
+    return element;
+};
+
+var appendInputOn = function(parent, type) {
+    var element = document.createElement("input");
+    element.setAttribute("type", type);
+    element.required = true;
+    parent.appendChild(element);
+    return element;
+};
