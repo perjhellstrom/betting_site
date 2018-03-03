a39ab69c40f6ec496972786db6fb92413b18bd59
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2018-03-03 17:08:04 +0100

    More work on creation panels and buttons

--- site/javascript/stage_creation.js
+++ site/javascript/stage_creation.js
@@ -26,34 +26,31 @@ var addStage = function() {
     var creationStageHeader = appendElementOn(creationStage, "div", "creation-stage-header");
     creationStageHeader.innerText = "Group Stage " + creationStageContainer.children.length;
 
-    var creationControlPanel = appendElementOn(creationStage, "div", "creation-control-panel");
-    var playersPerGroupElement = appendElementOn(creationControlPanel, "div", "creation-control-panel-element");
-    var advancingPlayersElement = appendElementOn(creationControlPanel, "div", "creation-control-panel-element");
-    var addGroupElement = appendElementOn(creationControlPanel, "div", "creation-control-panel-element");
-    var removeStageElement = appendElementOn(creationControlPanel, "div", "creation-control-panel-element");
+    var creationControlPanelColumn = appendElementOn(creationStage, "div", "creation-control-panel-column");
+    var playerPerGroupPanel = appendElementOn(creationControlPanelColumn, "div", "creation-control-panel");
+    var advancingPlayersPanel = appendElementOn(creationControlPanelColumn, "div", "creation-control-panel");
+    var buttonPanel = appendElementOn(creationControlPanelColumn, "div", "creation-control-panel");
 
-    appendElementOn(playersPerGroupElement, "label", "").innerText = "Players per group:";
-    var playersPerGroupInput = appendElementOn(playersPerGroupElement, "input", "");
+    appendElementOn(playerPerGroupPanel, "label", "").innerText = "Players per group:";
+    var playersPerGroupInput = appendElementOn(playerPerGroupPanel, "input", "");
     playersPerGroupInput.type = "number";
     playersPerGroupInput.min = "0";
 
-    appendElementOn(advancingPlayersElement, "label", "").innerText = "Advancing players:";
-    var advancingPlayersInput = appendElementOn(advancingPlayersElement, "input", "");
+    appendElementOn(advancingPlayersPanel, "label", "").innerText = "Advancing players:";
+    var advancingPlayersInput = appendElementOn(advancingPlayersPanel, "input", "");
     advancingPlayersInput.type = "number";
     advancingPlayersInput.min = "0";
 
-    var addGroupButton = appendElementOn(addGroupElement, "div", "green-button");
+    var addGroupButton = appendElementOn(buttonPanel, "div", "creation-stage-button green-button");
     addGroupButton.innerText = "Add Group";
     addGroupButton.onclick = addGroup;
 
-    var removeStageButton = appendElementOn(removeStageElement, "div", "red-button");
+    var removeStageButton = appendElementOn(buttonPanel, "div", "creation-stage-button red-button");
     removeStageButton.innerText = "Remove Stage";
     removeStageButton.onclick = removeStage;
 
     appendElementOn(creationStage, "div", "group-stage-container");
 };
-
-
 
 window.addEventListener("load", function(event) {
     var addStageButton = document.getElementById("add-stage");
